b334fea6ffbdfb6360402e7f87bf319a1d7d879c
TresforWrld <tresforzulu@gmail.com>
2025-12-11 22:54:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
+// Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"), (err) => {
+    if (err) {
+      console.error("Error sending index.html:", err);
+      res.status(500).send("Server error");
+    }
+  });
 });
 
+// Extra: log all static file requests
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
